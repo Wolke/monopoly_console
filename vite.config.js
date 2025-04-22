2ebc760fcf5792930864bc399e0c944a13d4bec5
wolke <wolkesau@gmail.com>
2025-04-22 10:32:27 +0800

    新增 gh-pages 套件以支援 GitHub Pages 部署，並更新 package.json、package-lock.json 及 vite.config.js 檔案以反映相關變更

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/monopoly_console/', // 設定專案的基礎路徑，需與 GitHub 專案名稱一致
 })
